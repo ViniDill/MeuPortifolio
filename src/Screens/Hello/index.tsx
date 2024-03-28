9f7fe29328ee3a6e544ef2a848c2a24e7531485e
Vinicius Dill Da Luz <vinidill9@gmail.com>
2024-03-28 14:44:58 -0300

    feat: add responsive

--- src/Screens/Hello/index.tsx
+++ src/Screens/Hello/index.tsx
@@ -16,6 +16,7 @@ const Hello: React.FC = () => {
                 <HeaderContainer>
                     <TextContainer>
                         <h1>Olá, eu sou Vinícius,</h1>
+                        <img src="./Images/dev.jpg" alt="Dev" className="Dev2" />
                         <h2>Desenvolvedor Web Front-End</h2>
                         <p className="paragraph" >
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quis saepe tenetur impedit atque enim blanditiis officia unde, praesentium molestias amet consequuntur ut neque at libero cupiditate omnis provident?
